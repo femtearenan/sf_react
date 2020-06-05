2cac89cd55f224c938099f795b18777996b3d5af
femtearenan <anders@femtearenan.se>
2020-06-05 19:22:39 +0200

    more jsx

--- assets/js/rep_log_react.js
+++ assets/js/rep_log_react.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
-const el = (<h2>Lift Stuff! <span>❤</span></h2>);
+const el = <h2>Lift Stuff! <span>❤</span></h2>
+
+
 
 ReactDom.render(el, document.getElementById('lift-stuff-app'));
