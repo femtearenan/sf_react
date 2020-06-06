4203a20031222b7d9a98768623d48467be11af74
Anders Björkland <anders@femtearenan.se>
2020-06-06 11:04:44 +0200

    sorted out React root component

--- assets/js/rep_log_react.js
+++ assets/js/rep_log_react.js
@@ -1,8 +1,5 @@
 import React from 'react';
-import ReactDom from 'react-dom';
+import { render } from 'react-dom';
+import RepLogApp from './RepLog/RepLogApp';
 
-const el = <h2>Lift Stuff! <span>❤</span></h2>
-
-
-
-ReactDom.render(el, document.getElementById('lift-stuff-app'));
+render(<RepLogApp />, document.getElementById('lift-stuff-app'));
