c4430e96bbdf3c3c07eb85b0fc7d94c1bc5f0186
Anders Björkland <anders@femtearenan.se>
2020-06-07 07:47:28 +0200

    refactored code into more components

--- assets/js/RepLog/RepLogApp.js
+++ assets/js/RepLog/RepLogApp.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import RepLogs from './RepLogs';
 
 export default class RepLogApp extends Component {
     constructor(props) {
@@ -7,9 +8,11 @@ export default class RepLogApp extends Component {
         this.state = {
             hightlightedRowId: null,
         }
+
+        this.handleRowClick = this.handleRowClick.bind(this);
     }
 
-    handleRowClick(repLogId, event) {
+    handleRowClick(repLogId) {
         this.setState({hightlightedRowId: repLogId});
     }
 
@@ -17,88 +20,12 @@ export default class RepLogApp extends Component {
         const { hightlightedRowId } = this.state;
         const { withHeart } = this.props;
 
-        let heart = '';
-        if (withHeart) {
-            heart = <span>❤</span>
-        }
-
-        const repLogs = [
-            { id: 1, reps: 25, itemLabel: 'My Laptop', totalWeightLifted: 112.5 },
-            { id: 2, reps: 10, itemLabel: 'Big Fat Cat', totalWeightLifted: 180 },
-            { id: 8, reps: 4, itemLabel: 'Big Fat Cat', totalWeightLifted: 72 }
-        ];
-
-
         return (
-            <div className="col-md-7">
-                <h2>
-                    Lift History {heart}
-                </h2>
-
-                <table className="table table-striped">
-                    <thead>
-                    <tr>
-                        <th>What</th>
-                        <th>How many times?</th>
-                        <th>Weight</th>
-                        <th>&nbsp;</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    {repLogs.map(repLog => (
-                        <tr 
-                            key={repLog.id} 
-                            className={hightlightedRowId===repLog.id ? 'info' : ''}
-                            onClick={(event) => this.handleRowClick(repLog.id, event) }
-                        >
-                            <td>{repLog.itemLabel}</td>
-                            <td>{repLog.reps}</td>
-                            <td>{repLog.totalWeightLifted}</td>
-                            <td>...</td>
-                        </tr>
-                    ))}
-                    </tbody>
-                    <tfoot>
-                        <tr>
-                            <td>&nbsp;</td>
-                            <th>Total</th>
-                            <th>TODO</th>
-                            <td>&nbsp;</td>
-                        </tr>
-                    </tfoot>
-                </table>
-
-                <form className="form-inline">
-                    <div className="form-group">
-                        <label className="sr-only control-label required" htmlFor="rep_log_item">
-                            What did you lift?
-                        </label>
-                        <select id="rep_log_item"
-                                name="item"
-                                required="required"
-                                className="form-control">
-                            <option value="">What did you lift?</option>
-                            <option value="cat">Cat</option>
-                            <option value="fat_cat">Big Fat Cat</option>
-                            <option value="laptop">My Laptop</option>
-                            <option value="coffee_cup">Coffee Cup</option>
-                        </select>
-                    </div>
-                    {' '}
-                    <div className="form-group">
-                        <label className="sr-only control-label required" htmlFor="rep_log_reps">
-                            How many times?
-                        </label>
-                        <input type="number" id="rep_log_reps"
-                            name="reps" required="required"
-                            placeholder="How many times?"
-                            className="form-control"/>
-                    </div>
-                    {' '}
-                    <button type="submit" className="btn btn-primary">I Lifted it!</button>
-                </form>
-
-            </div>
+            <RepLogs 
+                withHeart={withHeart} 
+                hightlightedRowId={hightlightedRowId} 
+                onRowClick={this.handleRowClick}
+            />
         );
     }
 }
---
+++ assets/js/RepLog/RepLogList.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+
+export default function RepLogList(props) {
+    const { hightlightedRowId, onRowClick } = props;
+
+    const repLogs = [
+        { id: 1, reps: 25, itemLabel: 'My Laptop', totalWeightLifted: 112.5 },
+        { id: 2, reps: 10, itemLabel: 'Big Fat Cat', totalWeightLifted: 180 },
+        { id: 8, reps: 4, itemLabel: 'Big Fat Cat', totalWeightLifted: 72 }
+    ];
+
+    return (
+        <tbody>
+        {repLogs.map(repLog => (
+            <tr 
+                key={repLog.id} 
+                className={hightlightedRowId===repLog.id ? 'info' : ''}
+                onClick={() => onRowClick(repLog.id) }
+            >
+                <td>{repLog.itemLabel}</td>
+                <td>{repLog.reps}</td>
+                <td>{repLog.totalWeightLifted}</td>
+                <td>...</td>
+            </tr>
+        ))}
+        </tbody>
+    );
+}
